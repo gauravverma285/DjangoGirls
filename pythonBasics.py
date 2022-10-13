b86171aca71367313975f6f566de7be4e1972ef0
gauravverma285 <gaurav998290@gmail.com>
2022-10-13 22:32:44 +0530

    main

---
+++ pythonBasics.py
@@ -0,0 +1,31 @@
+# dictionary
+
+# Empty dictionary
+# {}
+
+
+
+
+# participant = {'name' : 'Gaurav', 'country' : 'India', 'favourite_number' : [5,2,1]}
+# print(participant)
+
+# print(participant['name'])
+
+# participant['favourite_lang'] = 'python'
+# print(participant)
+
+# participant.pop('favourite_number')
+# print(participant)
+
+# participant['country'] = 'Nepal'
+# print(participant)
+
+
+                  # compare things
+
+# a =5
+# b =1
+# c = a>b
+# print(c)
+
+print("hello Gaurav")
